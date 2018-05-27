77c319bc7065e1711072a34bc3d0cbc4a7e44364
SergeyPanov <panovsy@gmail.com>
2018-05-27 15:25:52 +0200

    Done

--- main.go
+++ main.go
@@ -41,15 +41,17 @@ func readContent(path string)[][]float64  {
 func main() {
 	net := new(network.Network)
 
-	learningRate := flag.Float64("lr", 0.7, "Learning rate")
-	momentum := flag.Float64("m", 0.3, "Momentum")
-	inputLayer := flag.Int("il", 0, "Input layer")
-	hiddenLayer := flag.Int("hl", 0, "Hidden layer")
-	outputLayer := flag.Int("ol", 0, "Output layer")
+	learningRate := flag.Float64("lr", 0.7, "Learning rate.")
+	momentum := flag.Float64("m", 0.3, "Momentum.")
 
-	trainingSet := flag.String("ts", "./resources/wine-input", "Path to training set")
-	idealSet := flag.String("is", "./resources/wine-ideal", "Path to ideal set")
-	unknownSet := flag.String("us", "./resources/wine-unknown", "Path to unknown set")
+	hiddenLayer := flag.Int("hl", 0, "Hidden layer.")
+
+	trainingSet := flag.String("ts", "./resources/wine-input", "Path to training set.")
+	idealSet := flag.String("is", "./resources/wine-ideal", "Path to ideal set.")
+	unknownSet := flag.String("us", "./resources/wine-unknown", "Path to unknown set.")
+	epochs := flag.Int("ep", -1, "Stop condition is number of epochs.")
+	deviation := flag.Float64("dv", -1, "Stop condition is deviation.")
+
 
 	flag.Parse()
 	readContent(*trainingSet)
@@ -58,10 +60,18 @@ func main() {
 	is := readContent(*idealSet)
 	us := readContent(*unknownSet)
 
-	net.Init(*inputLayer, *hiddenLayer, *outputLayer, *learningRate, *momentum)
+	inputLayer := len(ts[0])
+	outputLayer := len(is[0])
 
 
-	net.LearnEpochs(100, ts, is)
+	net.Init(inputLayer, *hiddenLayer, outputLayer, *learningRate, *momentum)
+
+	if *epochs > 0 {
+		net.LearnEpochs(*epochs, ts, is)
+	}else if *deviation > 0 {
+		net.LearnError(*deviation, ts, is)
+	}
+
 
 	for i := 0; i < len(us);  i++ {
 		net.Calculate(us[i])
