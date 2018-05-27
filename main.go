f76872654bfdead9084485a14a0d471d46fbf82a
SergeyPanov <panovsy@gmail.com>
2018-05-27 13:39:13 +0200

    rootMSE calculation. Added learning condition

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ func main() {
 	net := new(network.Network)
 	net.Init(2, 2, 1, 0.7, 0.3)
 
-	for i := 0; i < 100 ; i ++  {
+	input := [][]float64{{1.0, 1.0}, {1.0, 0.0}, {0.0, 1.0}, {0.0, 0.0}}
+	ideal := [][]float64{{1.0}, {1.0}, {1.0}, {0.0}}
 
-		net.Calculate([]float64{1.0, 0.0})
-		net.Learn([]float64{0.0})
-	}
+	net.LearnEpochs(10000, input, ideal)
 
+	net.LearnError(0.01, input, ideal)
+	net.Calculate([]float64{0.0, 0.0})
 	fmt.Println(net.Result())
-
 
 }
--- network/network.go
+++ network/network.go
@@ -2,18 +2,24 @@ package network
 
 import (
 	"math/rand"
-	"fmt"
+	"time"
+	"math"
 )
 
 type Network struct {
 	input, hidden, output []Neuron
-	e, m float64
+	e, m, biasI, biasH    float64
+	avgRootMSE            float64
 }
 
 //Create input, hidden, output layers of neurons and connect them
-func (net *Network) Init(in, hd, ou int, e, m float64)  {
+func (net *Network) Init(in, hd, ou int, e, m float64) {
+	rand.Seed(time.Now().UTC().UnixNano())
 	net.e = e
 	net.m = m
+	net.avgRootMSE = 1
+	net.biasI = rand.Float64()
+	net.biasH = rand.Float64()
 	net.input = make([]Neuron, in)
 	net.hidden = make([]Neuron, hd)
 	net.output = make([]Neuron, ou)
@@ -43,23 +49,23 @@ func (net *Network) Calculate(vec []float64) []float64 {
 	outputVector := make([]float64, len(net.output))
 
 	//Fire of input neuron is an input vector
-	for i := range net.input{
+	for i := range net.input {
 		net.input[i].fire = vec[i]
 	}
 
 	//Calculate fires of hidden layer
-	for i, hn := range net.hidden{
-		inpValue := 0.0
-		for _, is := range hn.iSynapse{
+	for i, hn := range net.hidden {
+		inpValue := net.biasI
+		for _, is := range hn.iSynapse {
 			inpValue += is.fromNeuron.fire * is.weight
 		}
 		net.hidden[i].fire = hn.Sigmoid(inpValue)
 	}
 
 	//Calculate fire of output neuron
-	for i, on := range net.output{
-		inpValue := 0.0
-		for _, is := range on.iSynapse{
+	for i, on := range net.output {
+		inpValue := net.biasH
+		for _, is := range on.iSynapse {
 			inpValue += is.fromNeuron.fire * is.weight
 		}
 		net.output[i].fire = on.Sigmoid(inpValue)
@@ -67,86 +73,89 @@ func (net *Network) Calculate(vec []float64) []float64 {
 		outputVector[i] = on.fire
 	}
 	return outputVector
-
 }
 
 //Delta for output layer
-func (net *Network) deltaOut(ideal []float64) []float64 {
-	deltaO := make([]float64, len(net.output))
-	for i := 0; i < len(net.output); i++  {
-		deltaO[i] = (ideal[i] - net.output[i].fire) * net.output[i].DSigmoid()
+func (net *Network) deltaOut(ideal []float64) {
+	for i := 0; i < len(net.output); i++ {
 		net.output[i].delta = (ideal[i] - net.output[i].fire) * net.output[i].DSigmoid()
 	}
-	return deltaO
 }
+
 //Delta for hidden layer
 func (net *Network) deltaHidden(index int) {
 	sum := 0.0
-	for i := 0; i < len(net.hidden[index].oSynapse) ; i++ {
+	for i := 0; i < len(net.hidden[index].oSynapse); i++ {
 		sum += net.hidden[index].oSynapse[i].weight * net.hidden[index].oSynapse[i].toNeuron.delta
 	}
 	net.hidden[index].delta = sum * net.hidden[index].DSigmoid()
 }
 
 //Calculate gradient and delta, update weights base on calculated values
-func (net *Network) updateWeights(neuron *Neuron){
-	for i := 0; i < len(neuron.oSynapse) ; i++  {
+func (net *Network) updateWeights(neuron *Neuron) {
+	for i := 0; i < len(neuron.oSynapse); i++ {
 		grad := neuron.oSynapse[i].toNeuron.delta * neuron.fire
-		deltaW := net.e * grad + net.m * neuron.oSynapse[i].deltaPrevWeight
+		deltaW := net.e*grad + net.m*neuron.oSynapse[i].deltaPrevWeight
 		neuron.oSynapse[i].deltaPrevWeight = deltaW
 		neuron.oSynapse[i].weight += deltaW
 	}
 }
 
-func (net *Network) Learn(ideal []float64) {
+// Execute one step of learning process
+func (net *Network) learningStep(ideal []float64) {
 	net.deltaOut(ideal)
-	for i  := 0; i < len(net.hidden); i++ {
+	for i := 0; i < len(net.hidden); i++ {
 		net.deltaHidden(i)
 		net.updateWeights(&net.hidden[i])
 	}
 
-	for i := 0; i < len(net.input) ; i++  {
+	for i := 0; i < len(net.input); i++ {
 		net.updateWeights(&net.input[i])
 	}
 }
 
-// Return result
-func (net *Network) Result() []float64  {
-
-	res := make([]float64, len(net.output))
-
-	for i := 0; i < len(net.output) ; i++  {
-		res[i] = net.output[i].fire
+// Calculate root MSE
+func (net *Network) calculateRootMSE(ideal []float64) float64 {
+	divided := 0.0
+	for i := range ideal {
+		divided += math.Pow(ideal[i]-net.output[i].fire, 2)
 	}
-	return res
+	return math.Sqrt(divided / (float64(len(ideal))))
 }
 
+// Execute learning process. Stop condition is number of epochs
+func (net *Network) LearnEpochs(epoch int, input [][]float64, ideals [][]float64) {
 
-func (net *Network) Dump(){
-	fmt.Println("Input layer: ")
-	for i, in := range net.input{
-		fmt.Println("Fire of nuron: ", i, " is: ", in.fire)
-		fmt.Println("Output synapses:")
-		for _, os := range in.oSynapse{
-			fmt.Println("Weight: ", os.weight)
+	for e := 0; e < epoch; e++ {
+		totalError := 0.0
+		for i := 0; i < len(input); i++ {
+			net.Calculate(input[i])
+			totalError += net.calculateRootMSE(ideals[i])
+			net.learningStep(ideals[i])
 		}
+		net.avgRootMSE = totalError / float64(len(input))
 	}
+}
 
-	fmt.Println("----------------------------")
-	fmt.Println("Hidden layer: ")
+// Execute learning process. Stop condition is deviation
+func (net *Network) LearnError(deviation float64, input [][]float64, ideals [][]float64) {
 
-	for i, hn := range net.hidden{
-		fmt.Println("Fire of nuron: ", i, " is: ", hn.fire)
-		fmt.Println("Output synapses:")
-		for _, os := range hn.oSynapse{
-			fmt.Println("Weight: ", os.weight)
+	for net.avgRootMSE > deviation {
+		totalError := 0.0
+		for i := 0; i < len(input); i++ {
+			net.Calculate(input[i])
+			totalError += net.calculateRootMSE(ideals[i])
+			net.learningStep(ideals[i])
 		}
+		net.avgRootMSE = totalError / float64(len(input))
 	}
+}
 
-	fmt.Println("----------------------------")
-	fmt.Println("Output layer: ")
-	for i, on := range net.output{
-		fmt.Println("Fire of nuron: ", i, " is: ", on.fire)
+// Return result
+func (net *Network) Result() ([]float64, float64) {
+	res := make([]float64, len(net.output))
+	for i := 0; i < len(net.output); i++ {
+		res[i] = net.output[i].fire
 	}
-
+	return res, net.avgRootMSE
 }
